5ce2bc983bca3e044c2b724443c10309abb80f19
ihebghdifi <ihabghdifi@gmail.com>
2024-04-16 11:26:22 +0100

    done

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -6,9 +6,9 @@ export default function Home() {
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
         <h1>LOGIN</h1>
-       <Login/>
+       {/* <Login/> */}
        <h1>SIGNUP</h1>
-       <SignUp />
+       {/* <SignUp /> */}
       </div>
     </main>
   );
